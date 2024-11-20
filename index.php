4daf846682086560f51b30ae876bf324047271c1
Phitp <phitchakorn.jalasup@g.swu.ac.th>
2024-11-20 22:58:34 +0700

    before xammp

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SWU GAMES 2024</title>
 </head>
 <body>
+  <div class="main">
+    <div class="side-menu">
+
+    </div>
+    <div class="info">
+      
+    </div>
+  </div>
     
 </body>
 </html>
